5029e9d888b0aa24af7dc4cb703a85e8bf1834f8
Dim <dimitri.marion@gmail.com>
2026-04-02 23:32:43 +0200

    configure git

--- install.sh
+++ install.sh
@@ -163,5 +163,27 @@ echo "eval "'"$(starship init bash)"'"" >> ~/.bashrc
 # might be not necessary if you want to use the same locale for everything
 sudo update-locale LC_TIME=fr_FR.utf8 LC_PAPER=fr_FR.utf8 LC_NAME=fr_FR.utf8 LC_ADDRESS=fr_FR.utf8 LC_TELEPHONE=fr_FR.utf8 LC_MEASUREMENT=fr_FR.utf8
 
+
+# Configure git
+echo "Configuring git"
+read -p "Git user name: " git_user_name
+git config --global user.name $git_user_name
+read -p "Git email: " git_email
+git config --global user.email $git_email
+git config --global init.defaultBranc "master"
+
+# Github cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+gh auth login
+
 echo "disable power management on lid switch"
 echo " uncomment #HandleLidSwitch=ignore in /etc/systemd/logind.conf"
