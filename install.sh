49043fbc54d45df407dece100f84b6df0920ebca
Dim <dimitri.marion@gmail.com>
2026-04-03 22:36:23 +0200

    nextcloud

--- install.sh
+++ install.sh
@@ -193,6 +193,8 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo updates-alternatives --set x-www-browser /usr/bin/google-chrome-stable
 
+sudo apt install nextcloud-desktop
+
 # system wide darkmode
 # chrome
 # nextcloud
